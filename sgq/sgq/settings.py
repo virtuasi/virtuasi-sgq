c71171c43ad33176c64f10fd464db7bc816ed9b7
Matheus Lima <matheus.se@gmail.com>
2013-08-23 18:55:19 -0300

    add public app

--- sgq/sgq/settings.py
+++ sgq/sgq/settings.py
@@ -130,7 +130,8 @@ LOCAL_APPS = (
     'core',
     'apps.accounts',
     'apps.organization',
-    'apps.document', 
+    'apps.document',
+    'apps.public',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
